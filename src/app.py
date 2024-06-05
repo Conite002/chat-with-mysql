02694dc32a31960ba027289796b14a7631b6cee4
Conite002 <conite.gbodogbe@imsp-uac.org>
2024-06-05 09:32:11 +0100

    update app

--- src/app.py
+++ src/app.py
@@ -8,86 +8,86 @@ from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
 import streamlit as st
 
-# def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
-#   db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
-#   return SQLDatabase.from_uri(db_uri)
+def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
+  db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
+  return SQLDatabase.from_uri(db_uri)
 
-# def get_sql_chain(db):
-#   template = """
-#     You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
-#     Based on the table schema below, write a SQL query that would answer the user's question. Take the conversation history into account.
+def get_sql_chain(db):
+  template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+    Based on the table schema below, write a SQL query that would answer the user's question. Take the conversation history into account.
     
-#     <SCHEMA>{schema}</SCHEMA>
+    <SCHEMA>{schema}</SCHEMA>
     
-#     Conversation History: {chat_history}
+    Conversation History: {chat_history}
     
-#     Write only the SQL query and nothing else. Do not wrap the SQL query in any other text, not even backticks.
+    Write only the SQL query and nothing else. Do not wrap the SQL query in any other text, not even backticks.
     
-#     For example:
-#     Question: which 3 artists have the most tracks?
-#     SQL Query: SELECT ArtistId, COUNT(*) as track_count FROM Track GROUP BY ArtistId ORDER BY track_count DESC LIMIT 3;
-#     Question: Name 10 artists
-#     SQL Query: SELECT Name FROM Artist LIMIT 10;
+    For example:
+    Question: which 3 artists have the most tracks?
+    SQL Query: SELECT ArtistId, COUNT(*) as track_count FROM Track GROUP BY ArtistId ORDER BY track_count DESC LIMIT 3;
+    Question: Name 10 artists
+    SQL Query: SELECT Name FROM Artist LIMIT 10;
     
-#     Your turn:
+    Your turn:
     
-#     Question: {question}
-#     SQL Query:
-#     """
+    Question: {question}
+    SQL Query:
+    """
     
-#   prompt = ChatPromptTemplate.from_template(template)
+  prompt = ChatPromptTemplate.from_template(template)
   
-#   # llm = ChatOpenAI(model="gpt-4-0125-preview")
-#   llm = ChatGroq(model="mixtral-8x7b-32768", temperature=0)
+  # llm = ChatOpenAI(model="gpt-4-0125-preview")
+  llm = ChatGroq(model="mixtral-8x7b-32768", temperature=0)
   
-#   def get_schema(_):
-#     return db.get_table_info()
+  def get_schema(_):
+    return db.get_table_info()
   
-#   return (
-#     RunnablePassthrough.assign(schema=get_schema)
-#     | prompt
-#     | llm
-#     | StrOutputParser()
-#   )
+  return (
+    RunnablePassthrough.assign(schema=get_schema)
+    | prompt
+    | llm
+    | StrOutputParser()
+  )
     
-# def get_response(user_query: str, db: SQLDatabase, chat_history: list):
-#   sql_chain = get_sql_chain(db)
+def get_response(user_query: str, db: SQLDatabase, chat_history: list):
+  sql_chain = get_sql_chain(db)
   
-#   template = """
-#     You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
-#     Based on the table schema below, question, sql query, and sql response, write a natural language response.
-#     <SCHEMA>{schema}</SCHEMA>
+  template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+    Based on the table schema below, question, sql query, and sql response, write a natural language response.
+    <SCHEMA>{schema}</SCHEMA>
 
-#     Conversation History: {chat_history}
-#     SQL Query: <SQL>{query}</SQL>
-#     User question: {question}
-#     SQL Response: {response}"""
+    Conversation History: {chat_history}
+    SQL Query: <SQL>{query}</SQL>
+    User question: {question}
+    SQL Response: {response}"""
   
-#   prompt = ChatPromptTemplate.from_template(template)
+  prompt = ChatPromptTemplate.from_template(template)
   
-#   # llm = ChatOpenAI(model="gpt-4-0125-preview")
-#   llm = ChatGroq(model="mixtral-8x7b-32768", temperature=0)
+  # llm = ChatOpenAI(model="gpt-4-0125-preview")
+  llm = ChatGroq(model="mixtral-8x7b-32768", temperature=0)
   
-#   chain = (
-#     RunnablePassthrough.assign(query=sql_chain).assign(
-#       schema=lambda _: db.get_table_info(),
-#       response=lambda vars: db.run(vars["query"]),
-#     )
-#     | prompt
-#     | llm
-#     | StrOutputParser()
-#   )
+  chain = (
+    RunnablePassthrough.assign(query=sql_chain).assign(
+      schema=lambda _: db.get_table_info(),
+      response=lambda vars: db.run(vars["query"]),
+    )
+    | prompt
+    | llm
+    | StrOutputParser()
+  )
   
-#   return chain.invoke({
-#     "question": user_query,
-#     "chat_history": chat_history,
-#   })
+  return chain.invoke({
+    "question": user_query,
+    "chat_history": chat_history,
+  })
     
   
-# if "chat_history" not in st.session_state:
-#     st.session_state.chat_history = [
-#       AIMessage(content="Hello! I'm a SQL assistant. Ask me anything about your database."),
-#     ]
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+      AIMessage(content="Hello! I'm a SQL assistant. Ask me anything about your database."),
+    ]
 
 load_dotenv()
 
@@ -105,35 +105,35 @@ with st.sidebar:
     st.text_input("Password", type="password", value="admin", key="Password")
     st.text_input("Database", value="Chinook", key="Database")
     
-#     if st.button("Connect"):
-#         with st.spinner("Connecting to database..."):
-#             db = init_database(
-#                 st.session_state["User"],
-#                 st.session_state["Password"],
-#                 st.session_state["Host"],
-#                 st.session_state["Port"],
-#                 st.session_state["Database"]
-#             )
-#             st.session_state.db = db
-#             st.success("Connected to database!")
+    if st.button("Connect"):
+        with st.spinner("Connecting to database..."):
+            db = init_database(
+                st.session_state["User"],
+                st.session_state["Password"],
+                st.session_state["Host"],
+                st.session_state["Port"],
+                st.session_state["Database"]
+            )
+            st.session_state.db = db
+            st.success("Connected to database!")
     
-# for message in st.session_state.chat_history:
-#     if isinstance(message, AIMessage):
-#         with st.chat_message("AI"):
-#             st.markdown(message.content)
-#     elif isinstance(message, HumanMessage):
-#         with st.chat_message("Human"):
-#             st.markdown(message.content)
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.markdown(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.markdown(message.content)
 
-# user_query = st.chat_input("Type a message...")
-# if user_query is not None and user_query.strip() != "":
-#     st.session_state.chat_history.append(HumanMessage(content=user_query))
+user_query = st.chat_input("Type a message...")
+if user_query is not None and user_query.strip() != "":
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
     
-#     with st.chat_message("Human"):
-#         st.markdown(user_query)
+    with st.chat_message("Human"):
+        st.markdown(user_query)
         
-#     with st.chat_message("AI"):
-#         response = get_response(user_query, st.session_state.db, st.session_state.chat_history)
-#         st.markdown(response)
+    with st.chat_message("AI"):
+        response = get_response(user_query, st.session_state.db, st.session_state.chat_history)
+        st.markdown(response)
         
-#     st.session_state.chat_history.append(AIMessage(content=response))
+    st.session_state.chat_history.append(AIMessage(content=response))
